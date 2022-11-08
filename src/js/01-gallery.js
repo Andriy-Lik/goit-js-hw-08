0ef66f7d58b2877306e07b26eaf38f23c038b404
Andriy-Lik <alan321987@gmail.com>
2022-11-08 23:52:19 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -14,11 +14,9 @@ tagContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 function createImagesCardsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
-        <li>
             <a class="gallery__item" href="${original}">
                 <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
-        </li>
         `;
     }).join('');    
 }
